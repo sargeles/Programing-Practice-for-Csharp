0a15f416b4e3fd49384fc07583f5706ec64485b0
sargeles <330657387@qq.com>
2016-10-18 10:14:09 +0800

    2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int i = 1;
+            int i = 2;
             Console.WriteLine(i);
         }
     }
